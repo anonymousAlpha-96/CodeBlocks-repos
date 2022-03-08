4c9a2b9ad323344826afb93b6debcdfb5e36f1f9
Ankit <43381040+anonymousAlpha-96@users.noreply.github.com>
2022-03-08 21:20:46 +0530

    Update variableShadowing.cpp

--- variableShadowing.cpp
+++ variableShadowing.cpp
@@ -5,17 +5,12 @@ using namespace std;
 int main()
 {
     int nameHiding = 18;
-
     {
-        cout<<"NameHiding value is : "<<nameHiding<<endl;
+        cout<<"Global Name Hiding value inside : "<<nameHiding<<endl;
 
         int nameHiding = 25;
-
-        //nameHiding = 25;
-
         cout<<"Name Hiding after inner initialization : "<<nameHiding<<endl;
     }
     cout<<"name hiding value in outer side : "<<nameHiding<<endl;
-
     return 0;
 }
