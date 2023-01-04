e5add3f6d933a97b9f75a6e2a156a8f59f21d09f
Ankit <43381040+anonymousAlpha-96@users.noreply.github.com>
2023-01-04 19:42:29 +0530

    Add files via upload

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,51 +1,69 @@
-//Selection sort
+/*
+	* selection sort Algorithm
+	* Tested Okay
+*/
 
 #include<iostream>
+#include<algorithm>
 
 using namespace std;
 
+//function declaration
+void sortPrint(int a[], int asize);
 
-int selectionSort(int a[], int n)
-{
-    for(int i = 0;i<n;i++)
-    {
-        int minIndex = i;
-
-        for(int j = i+1;j<n;j++)
-        {
-            if(a[j]<a[minIndex])
-            {
-                minIndex = j;
-            }
-        }
-        swap(a[minIndex],a[i]);
-    }
-}
-
-int printArray(int a[], int n)
-{
-    cout<<"Sorted Array Elements are : ";
-    for(int i = 0;i<n;i++)
-    {
-        cout<<a[i]<<" ";
-    }
-}
-
-
+//main part
 int main()
 {
-    int Size;
-    int arr[10];
+	int a_size;
+	int temp;
+	int j, min, k;
+	int array[10];
 
-    cout<<"Enter the Size of an Array : ";
-    cin>>Size;
+	cout<<"Enter the Size of array : ";
+	cin>>a_size;
 
-    cout<<"Enter the Array Elements : ";
-    for(int i = 0; i<Size; i++)
-    {
-        cin>>arr[i];
-    }
+	//user input to array
+	cout<<"\nEnter the elements : ";
+	for(int i = 0;i<a_size;i++)
+	{
+		cin>>array[i];
+	}
 
-    selectionSort(arr, Size);
-    printArray(arr, Size);
+	//calls the print array function before SORT
+	cout<<"Array values before sorting : "<<endl;
+	sortPrint(array, a_size);
+
+	//selection sort algorithm
+	for(int i = 0;i<a_size - 1; i++)
+	{
+		min = array[i];
+		k = i;
+		for(j = i+1;j<a_size;j++)
+		{
+			if(min>array[j])
+			{
+				min = array[j];
+				k = j;
+			}
+		}
+		temp = array[i];
+		array[i] = array[k];
+		array[k] = temp;
+	}
+
+	//calls the print array function after SORT
+	cout<<"\nArray values after sorting : "<<endl;
+	sortPrint(array, a_size);
+	cout<<endl;
+
+	return 0;
+
+}
+//function to display the array elements
+void sortPrint(int a[], int asize)
+{
+	for(int i  = 0; i<asize; ++i) //++i
+	{
+		cout<<a[i]<<" ";
+	}
 }
